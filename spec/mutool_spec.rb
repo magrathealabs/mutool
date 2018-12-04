73f76cae7449812e566dce45a2d5d8079e989658
Danilo Jeremias da Silva <daniloj.dasilva@gmail.com>
2018-12-04 14:19:36 -0200

    Remove shell injection

--- spec/mutool_spec.rb
+++ spec/mutool_spec.rb
@@ -1,11 +1,24 @@
+require 'pathname'
+
 RSpec.describe Mutool do
+  let(:tmp) { Pathname.new('.').join('spec', 'tmp') }
+  let(:pattern) { 'spec/tmp/convert-test-%d.png' }
+
   it 'has a version number' do
     expect(Mutool::VERSION).not_to be nil
   end
 
   it '#convert' do
-    expect(File.exist?('spec/tmp/convert-test-1.png')).to eq(false)
-    Mutool.convert('spec/resources/pdf.pdf', {F: 'png', o: 'spec/tmp/convert-test-%d.png'})
-    expect(File.exist?('spec/tmp/convert-test-1.png')).to eq(true)
+    expect(tmp.join('convert-test-1.png').exist?).to be_falsey
+    Mutool.convert('spec/resources/pdf.pdf', { F: 'png', p: "'; touch abc.png;'", o: pattern })
+    expect(tmp.join('convert-test-1.png').exist?).to be_thruth
+    expect(File.exist?('abc.png').exist?).to be_falsey
+    expect(tmp.join('abc.png').exist?).too be_falsey
+  end
+
+  it '#convert with password' do
+    expect(tmp.join('convert-test-password-1.png').exist?).to be_falsey
+    Mutool.convert('spec/resources/pdf_with_password.pdf', { F: 'png', p: 'abc', o: pattern })
+    expect(tmp.join('convert-test-password-1.png').exist?).to be_thruth
   end
 end
