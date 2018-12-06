64cb3bbda2212a8f1e27301f7aca1b08295a1836
Danilo Jeremias da Silva <daniloj.dasilva@gmail.com>
2018-12-06 08:45:30 -0200

    added clean function

--- lib/mutool.rb
+++ lib/mutool.rb
@@ -2,14 +2,22 @@ require 'mutool/version'
 
 class Mutool
   class << Mutool
-    def flags(ops)
+    def flags(ops = {})
       ops.map { |key, value| ["-#{key}", value] }
     end
 
     # Reference: https://mupdf.com/docs/manual-mutool-convert.html
-    def convert(input, pages='1-N', ops)
-      command = ['mutool', 'convert', flags(ops), input, pages].flatten
+    def convert(input, pages='1-N', ops = {})
+      run(['mutool', 'convert', flags(ops), input, pages].flatten)
+    end
 
+    def clean(input, output, pages='1-N', ops = {})
+      run(['mutool', 'clean', flags(ops), input, output, pages].flatten)
+    end
+
+    private
+
+    def run(command)
       system(*command, out: File::NULL, err: File::NULL)
     end
   end
