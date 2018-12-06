4f5f5b1ecb1968cf63cecbd6dc82a530306112cc
Danilo Jeremias da Silva <daniloj.dasilva@gmail.com>
2018-12-06 13:14:45 -0200

    Update defaluts

--- lib/mutool.rb
+++ lib/mutool.rb
@@ -7,17 +7,17 @@ class Mutool
     end
 
     # Reference: https://mupdf.com/docs/manual-mutool-convert.html
-    def convert(input, pages='1-N', ops = {})
+    def convert(input, pages='1-N', ops)
       run(['mutool', 'convert', flags(ops), input, pages].flatten)
     end
 
-    def clean(input, output, pages='1-N', ops = {})
+    def clean(input, output, pages='1-N', ops)
       run(['mutool', 'clean', flags(ops), input, output, pages].flatten)
     end
 
     private
 
-    def run(command)
+    def run(command = [])
       system(*command, out: File::NULL, err: File::NULL)
     end
   end
